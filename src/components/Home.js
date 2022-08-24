82f5438b388ac6e53a7da6cb70ac6f248f011f4e
mk-hasan <mkhasan6891@gmail.com>
2022-08-24 13:54:45 +0600

    handleUpdate created for update to db

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { db } from '../firebase.init';
 import { uid } from 'uid';
-import { onValue, ref, remove, set } from 'firebase/database';
+import { onValue, ref, remove, set, update } from 'firebase/database';
 const Home = () => {
     const [task, setTask] = useState('');
     const [tasks, setTasks] = useState([]);
+    const [isEdit, setIsEdit] = useState(false);
+    const [tempUuid, setTempUuid] = useState('');
 
+    const handleTaskChange = (e) => {
+        setTask(e.target.value);
+    }
     const createTodo = (e) => {
         e.preventDefault();
         const uuid = uid();
@@ -35,19 +40,44 @@ const Home = () => {
     const handleDelete = (task) => {
         remove(ref(db, `/${task.uuid}`));
     }
+    // update  
+    const handleUpdate = (task) => {
+        setIsEdit(true);
+        setTempUuid(task.uuid);
+        setTask(task.task);
 
+    }
+    const handleUpdateSubmit = () => {
+        update(ref(db, `/${tempUuid}`), {
+            task,
+            uuid: tempUuid,
+        });
+        setTask('');
+        setIsEdit(false);
+    }
 
     return (
         <div>
             <div className='flex items-center mt-12 flex-col'>
                 <h3 className='text-3xl font-extrabold mb-2 font-mono'>Add Task</h3>
                 <div className="form-control w-full md:w-1/2">
-                    <form onSubmit={createTodo} className="input-group ">
-                        <input type="text" name='task' value={task} onChange={handleChange} placeholder="Write task" className="input w-full text-2xl focus:outline-0 input-bordered" />
-                        <button type='submit' className="btn btn-square input-bordered bg-white hover:bg-[#F44336] hover:input-bordered">
-                            Add
-                        </button>
-                    </form>
+                    <input type="text" value={task} onChange={handleTaskChange} />
+                    <input type="text" name='task' value={task} onChange={handleChange} placeholder="Write task" className="input w-full text-2xl focus:outline-0 input-bordered" />
+                    {isEdit ? (
+                        <>
+                            <button onClick={handleUpdateSubmit}>Submit Change</button>
+                            <button
+                                onClick={() => {
+                                    setIsEdit(false);
+                                    setTask("");
+                                }}
+                            >
+                                X
+                            </button>
+                        </>
+                    ) : (
+                        <button onClick={createTodo}>submit</button>
+                    )}
                 </div>
             </div>
             <div className='px-2 md:w-1/2 mx-auto'>
@@ -62,7 +92,7 @@ const Home = () => {
                                     {tasks.map(task => (
                                         <>
                                             <h1>{task.task}</h1>
-                                            <button className='btn btn-primary'>Update</button>
+                                            <button onClick={() => handleUpdate(task)} className='btn btn-primary'>Update</button>
                                             <button onClick={() => handleDelete(task)} className='btn btn-primary'>Delete</button>
                                         </>
                                     ))}
